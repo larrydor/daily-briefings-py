0f0779d891b8371b45356a6037dbf60105fafb95
larrydor <86853201+larrydor@users.noreply.github.com>
2021-07-30 21:49:47 -0400

    Render HTML pages

--- web_app/routes/home_routes.py
+++ web_app/routes/home_routes.py
@@ -8,14 +8,14 @@ home_routes = Blueprint("home_routes", __name__)
 @home_routes.route("/home")
 def index():
     print("HOME...")
-    return "Welcome Home"
-    #return render_template("home.html")
+    #return "Welcome Home"
+    return render_template("home.html")
 
 @home_routes.route("/about")
 def about():
     print("ABOUT...")
-    return "About Me"
-    #return render_template("about.html")
+    #return "About Me"
+    return render_template("about.html")
 
 
 # web_app/routes/home_routes.py
@@ -28,8 +28,8 @@ def hello_world():
     # NOTE: `request.args` is dict-like, so below we're using the dictionary's `get()` method,
     # ... which will return None instead of throwing an error if key is not present
     # ... see also: https://www.w3schools.com/python/ref_dictionary_get.asp
-    name = request.args.get("name") or "World"
+    name = request.args.get("name") or "Larry"
     message = f"Hello, {name}!"
-    return message
-    #return render_template("hello.html", message=message)
+    #return message
+    return render_template("hello.html", message=message)
 
